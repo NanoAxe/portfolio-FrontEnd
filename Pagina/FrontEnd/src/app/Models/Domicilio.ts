9f4ef216eef55b43c3c6afe7bc3cee21f130bfb4
Miguel Barconte <timoteoalexblade@gmail.com>
2023-09-25 20:49:52 -0300

    Eliminando carpeta: Pagina

--- Pagina/FrontEnd/src/app/Models/Domicilio.ts
+++
@@ -1,9 +0,0 @@
-export class Domicilio{
-    id_d: number | undefined;
-    pais: string | undefined;
-    provincia: string | undefined;
-    barrio: string | undefined;
-    calle: string | undefined;
-    numero: number | undefined;
-    id_p: number | undefined;
-}
--- Pagina/FrontEnd/src/app/Models/Educacion.ts
+++
@@ -1,9 +0,0 @@
-export class Educacion{
-    id_E: number | undefined;
-    img_insti: string | undefined;
-    institucion: string | undefined;
-    periodo_f: number | undefined;
-    periodo_i: number | undefined;
-    titulo: string | undefined;
-    id_u: number | undefined;
-}
